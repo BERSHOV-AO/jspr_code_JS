7ee8c76318cfcdf0d59142751a451b4123cc7b23
Aleksey Bershov <bershov.aleksey@bk.ru>
2023-10-07 14:18:50 +0300

    Writing a commentary Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,10 +16,17 @@ public class Main {
     private static final Logger LOGGER = Grizzly.logger(Main.class);
 
     public static void main(String[] args) throws IOException, InterruptedException {
+        // Сервер создается вызовом статического метода, createSimpleServer(),
+        // в аргументах которого, указываем - docRoot "static" - это папка, в которой хранятся статические файлы
+        // которые будут отдаваться автоматом (без изменения), без каких либо добавлений обработчиков.
         final var server = HttpServer.createSimpleServer("static", 9999);
+        // Что бы добавить обработчик(Handler), нам нужно создать new HttpHandler
         server.getServerConfiguration().addHttpHandler(new HttpHandler() {
             @Override
+            // будут приходить два объекта request, response
             public void service(Request request, Response response) throws Exception {
+                // если мы хотим клиенту какую либо информацию отправить, мы будем делать это через response
+                // мы отправим клиенту body из двух символов "ok"
                 response.getWriter().write("ok");
                 //        response.suspend();
                 //        MultipartScanner.scan(request, multipartEntry -> {
@@ -41,10 +48,14 @@ public class Main {
                 //          }
                 //        });
             }
+            // указываем mapping, по какому адресу будет срабатывать этот обработчик
         }, "/api");
-
+        // запуск сервера
         server.start();
+        // Что бы сервер выключился корректно мы можем подписаться,
+        // на завершение работы Runtime.getRuntime().addShutdownHook
         Runtime.getRuntime().addShutdownHook(new Thread(server::shutdown));
+        // Что бы у нас консоль не вылетела, мы ее блокируем
         Thread.currentThread().join();
     }
 }
